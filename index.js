e66bf068f53358e70e5064cfc052b0f0e6d39008
simon1re <109280082+simon1re@users.noreply.github.com>
2023-01-08 10:54:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ import { catsData } from '/data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
 
+/*
+Challenge:
+1. Refactor this nested for of so that an 
+   emotion is only pushed to emotionsArray
+   if it is not already in emotionsArray.
+   Extra kudos if you use the "logical not"
+   operator - feel free to google it!
+*/
+
 function getEmotionsArray(cats){
     const emotionsArray = []
     for (let cat of cats){
         for (let emotion of cat.emotionTags){
-            emotionsArray.push(emotion)
+            if (!emotionsArray.includes(emotion)) {
+                emotionsArray.push(emotion)
+            } 
         }
     }
     return emotionsArray
@@ -16,7 +27,6 @@ function renderEmotionsRadios(cats){
     let radioItems = ``
     const emotions = getEmotionsArray(cats)
     for (let emotion of emotions){
-
 
         radioItems += ` 
         <div class="radio">
